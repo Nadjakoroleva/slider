5bda2a82ffaa8c667bdd55d89534df6bdd5e5393
Nadjakoroleva <nadin.koroleva@gmail.com>
2018-04-08 14:21:11 +0300

    1

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      arrows: true,
+      slidesToScroll: 1,
+      nextArrow: '<button class="arrow slider-arrow__left"></button>',
+      prevArrow: '<button class="arrow slider-arrow__right"></button>',
+    });
+  });
+
+ 
